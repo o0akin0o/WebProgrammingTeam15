cc058efab468fbf5915ea52faf5e87060276006d
dandev947366 <dan.dev947366@gmail.com>
2024-02-03 23:35:10 +0200

    update

---
+++ header.php
@@ -0,0 +1,23 @@
+<nav class="navbar row">
+                <h2 class="col-md-4 logo">Eastern Kitchen</h2>
+                <div class="col-md-8">
+
+                    <ul>
+                        <li>
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="ex1.php">Ex1</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="ex2.php">Ex2</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="ex3.php">Ex3</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="ex4.php">Ex4</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
