c6eb30287864358709931a9a7baec4902af6fb52
dandev947366 <dan.dev947366@gmail.com>
2024-02-08 19:50:59 +0200

    update

--- src/pages/links.php
+++ src/pages/links.php
@@ -6,3 +6,4 @@
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;1,400;1,600&family=Montserrat:ital,wght@0,300;0,400;0,700;1,500&display=swap" rel="stylesheet">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
