be591416a72f33c45f3618debb4256167ec69c5c
dandev947366 <dan.dev947366@gmail.com>
2024-02-08 09:41:40 +0200

    update login_nav

--- src/pages/login_nav.php
+++ src/pages/login_nav.php
@@ -1,7 +1,7 @@
 <?php include('links.php'); ?>
 <div class="container">
-<div class="container mx-auto my-4">
-        <a id="login" class="p-3"  href="login.php"><i class="fa sign-in">Login</i></a>
-        <a href="#" class="p-3" ><i class="fa fa-sign-out">Logout</i></a> 
+<div class="container mx-auto my-4 login_nav">
+        <a id="login" class="p-3 link-success"  href="login.php"><i class="fa sign-in">Login</i></a>
+        <a href="#" class="p-3 link-secondary" ><i class="fa fa-sign-out">Logout</i></a> 
     </div>
 </div>
