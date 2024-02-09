97914ea996c65a58387176028ab9023f42999249
o0akin0o <lenhatminhquan@gmail.com>
2024-02-09 11:10:35 +0200

    clear customersid when logout

--- src/pages/logout.php
+++ src/pages/logout.php
@@ -2,6 +2,7 @@
 // Logout
     // Delete cookie by set time exp to the past
     setcookie("name", "", time() - (86400 * 30), "/");
+    setcookie("customersid", "", time() - (86400 * 30), "/");
     // Redirect to prev page
     $prevPage = $_COOKIE['prev_page'];
     header("Location: $prevPage");
