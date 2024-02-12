ea63f65f3a002cbce340136e4ad6200b58ead203
dandev947366 <dan.dev947366@gmail.com>
2024-02-12 10:01:20 +0200

    update

--- src/pages/cart.php
+++ src/pages/cart.php
@@ -57,10 +57,9 @@ if (isset($_GET['action'])) {
         header('Location: ./cart.php');
 
       } elseif (isset($_POST['order_click'])) {
-        if(empty($_POST['qty'])){
+        if (empty($_POST['qty'])) {
           $error = "Your cart is empty";
-        }
-        elseif (empty($_POST['name']) && !empty($_POST['qty'])) {
+        } elseif (empty($_POST['name']) && !empty($_POST['qty'])) {
           $error = "You did not input name";
 
         } elseif (empty($_POST['phone']) && !empty($_POST['qty'])) {
@@ -69,42 +68,42 @@ if (isset($_GET['action'])) {
         } elseif (empty($_POST['address']) && !empty($_POST['qty'])) {
           $error = "You did not input address";
 
-        } 
-        elseif ($error==false && !empty($_POST['qty'])){
-          $total=0;
+        } elseif ($error == false && !empty($_POST['qty'])) {
+          $total = 0;
           $name = $_POST['name'];
           $phone = $_POST['phone'];
           $address = $_POST['address'];
           $note = $_POST['note'];
-          
-          
+
+
           $join = implode(",", array_keys($_POST['qty']));
           $products = mysqli_query($conn, "SELECT * FROM `Products` WHERE `id` IN ($join)");
-         
-          while ($row = mysqli_fetch_array($products)) { 
+
+          while ($row = mysqli_fetch_array($products)) {
             $total += $row['price'] * $_POST['qty'][$row['id']];
-            $proId= $row['id'];
+            $proId = $row['id'];
             $proQty = $_POST['qty'][$row['id']];
             $proPrice = $row['price'];
-             
-       
+
+
           }
-  
+         
           $insertOrder = mysqli_query($conn, "INSERT INTO `Orders` (`id`, `name`, `phone`, `address`, `note`, `total`, `created_time`, `last_updated`, `customer_id`, `status`) VALUES (NULL, '$name', '$phone', '$address', '$note', '$total', NOW(), NOW() , NULL, 'Pending')");
-          $orderId= $conn->insert_id;
+          $orderId = $conn->insert_id;
+
          
           $insertOrderDetails = mysqli_query($conn, "INSERT INTO `Order_Details` (`id`, `order_id`, `product_id`, `quantity`, `price`, `created_time`, `last_updated`) VALUES (NULL, '$orderId', '$proId', '$proQty', '$proPrice', NOW(), NOW())");
-         
-        if ($insertOrderDetails) {
-  
-         unset($_SESSION['cart']);
-          header("location: thankyou.php");
-        }
+
+          if ($insertOrderDetails) {
+
+            unset($_SESSION['cart']);
+            header("location: thankyou.php");
+          }
 
         }
-        }
-      }  
+      }
   }
+}
 
 if (!empty($_SESSION['cart'])) {
   $join = implode(",", array_keys($_SESSION['cart']));
@@ -133,81 +132,87 @@ if (!empty($_SESSION['cart'])) {
     <?php include 'login_nav.php'; ?>
     <?php include 'mycart.php'; ?>
     <div class="container">
-      
+
       <!-- FORM -->
       <form action='cart.php?action=submit' method='post'>
-          
+
         <div class="form-group row">
           <div class="jumbotron row mx-auto my-4">
             <h1 class="display-3">My Cart</h1>
-            
-            <hr class="my-4">
-            <table class="table table-bordered table-responsive table-striped">
-              <thead class="thead-dark">
-                <tr>
-                  <th scope="" class="col-sm-1"></th>
-                  <th scope="" class="col-sm-3">Name</th>
-                  <th scope="" class="col-sm-2">Price</th>
-                  <th scope="" class="col-sm-1">Quantity</th>
-                  <th scope="" class="col-sm-3">Total</th>
-                  <th scope="" class="col-sm-1"></th>
-                  <th scope="" class="col-sm-1"></th>
-                </tr>
-              </thead>
+            <?php
+            if ($products !== null && mysqli_num_rows($products) > 0) {
+              echo '<hr class="my-4">';
+              echo '<table class="table table-bordered table-responsive table-striped">';
+              echo '<thead class="thead-dark">';
+              echo '<tr>';
+              echo '<th scope="" class="col-sm-1"></th>';
+              echo '<th scope="" class="col-sm-3">Name</th>';
+              echo '<th scope="" class="col-sm-2">Price</th>';
+              echo '<th scope="" class="col-sm-1">Quantity</th>';
+              echo '<th scope="" class="col-sm-3">Total</th>';
+              echo '<th scope="" class="col-sm-1"></th>';
+              echo '<th scope="" class="col-sm-1"></th>';
+              echo '</tr>';
+              echo '</thead>';
+            }
+            ?>
 
+            <tbody>
+              <!-- WHILE LOOP -->
+              <?php
+              $num = 1;
+              $total = 0;
+              if ($products !== null && mysqli_num_rows($products) > 0) {
+                while ($row = mysqli_fetch_array($products)) { ?>
 
-              <tbody>
-                <!-- WHILE LOOP -->
-                <?php
-                $num = 1;
-                $total= 0;
-                if ($products !== null && mysqli_num_rows($products) > 0) {
-                  while ($row = mysqli_fetch_array($products)) { ?>
+                  <tr>
+                    <td>
+                      <?= $num; ?>
+                    </td>
+                    <td>
+                      <?= $row['name']; ?>
+                    </td>
+                    <td>
+                      <?= $row['price']; ?> €
+                    </td>
+                    <td>
+                      <input type="number" value="<?= $_SESSION['cart'][$row['id']] ?>" class="form-control"
+                        aria-label="Small" aria-describedby="inputGroup-sizing-sm" name="qty[<?= $row['id']; ?>]">
 
-                    <tr>
-                      <td>
-                        <?= $num; ?>
-                      </td>
-                      <td>
-                        <?= $row['name']; ?>
-                      </td>
-                      <td>
-                        <?= $row['price']; ?> €
-                      </td>
-                      <td>
-                        <input type="number" value="<?= $_SESSION['cart'][$row['id']] ?>" class="form-control"
-                          aria-label="Small" aria-describedby="inputGroup-sizing-sm" name="qty[<?= $row['id']; ?>]">
-
-                      </td>
-                      <td>
-                      <?= number_format(($row['price'] *  $_SESSION['cart'][$row['id']]), 0, ",", "."); ?> €
-                      </td>
-                      <td><input type="submit" class="btn btn-outline-success submit" name="update_click" value="Update"></input>
-                      </td>
-                      <td> <a class="btn btn-outline-danger" href="cart.php?action=delete&id=<?= $row['id'] ?>">Delete</a></td>
-                    </tr>
-                
-                    <?php
-                    $num++;
-                    $total += ($row['price'] *  $_SESSION['cart'][$row['id']]);
-                  }
-                  ?>
-                  <th scope="row"></th>
-                  <td><strong>Total</strong></td>
-                  <td></td>
-                  <td></td>
-                  <td><strong><?= number_format($total, 0, ",", ".") ?> €</strong></td>
-                  <td></td>
+                    </td>
+                    <td>
+                      <?= number_format(($row['price'] * $_SESSION['cart'][$row['id']]), 0, ",", "."); ?> €
+                    </td>
+                    <td><input type="submit" class="btn btn-outline-success submit" name="update_click"
+                        value="Update"></input>
+                    </td>
+                    <td> <a class="btn btn-outline-danger" href="cart.php?action=delete&id=<?= $row['id'] ?>">Delete</a>
+                    </td>
                   </tr>
-                </tbody>
+
+                  <?php
+                  $num++;
+                  $total += ($row['price'] * $_SESSION['cart'][$row['id']]);
+                }
+                ?>
+                <th scope="row"></th>
+                <td><strong>Total</strong></td>
+                <td></td>
+                <td></td>
+                <td><strong>
+                    <?= number_format($total, 0, ",", ".") ?> €
+                  </strong></td>
+                <td></td>
+                </tr>
+              </tbody>
               </table>
 
               <?php
 
-                } else {
+              } else {
 
-                  echo "Your cart is empty.";
-                } ?>
+                echo "Your cart is empty.";
+              } ?>
 
 
 
@@ -229,78 +234,78 @@ if (!empty($_SESSION['cart'])) {
 
           <!-- END FORM -->
           <!-- END CART -->
-          
-          
 
-              <!-- CUSTOMER DETAIL FORM -->
-              <?php if (!empty($error)) { ?>
-        <div id="notify-msg">
-          <?= $error ?>. <a href="menu.php">Continue Shopping</a>
+
+
+          <!-- CUSTOMER DETAIL FORM -->
+          <?php if (!empty($error)) { ?>
+            <div id="notify-msg">
+              <?= $error ?>. <a href="menu.php">Continue Shopping</a>
+            </div>
+          <?php } else { ?>
+
+          <?php } ?>
+          <div class="booking">
+            <div class="row">
+              <div class="col-md-5"></div>
+              <div class="col-md-2">
+                <h3 class="booking-title">Contact Information</h3>
+
+              </div>
+
+
+              <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                  <form id="cart-form" action="cart.php?action=submit">
+                    <fieldset>
+                      <div class="form-group">
+                        <label for="" class="p-2">Name</label>
+                        <input type="text" id="" class="form-control" placeholder="Name" name="name">
+                      </div>
+                      <div class="form-group">
+                        <label for="" class="p-2">Phone</label>
+                        <input type="text" id="" class="form-control" placeholder="Phone" name="phone">
+                      </div>
+                      <div class="form-group">
+                        <label for="" class="p-2">Address</label>
+                        <input type="text" id="" class="form-control" placeholder="123 Lesmurine" name="address">
+                      </div>
+
+                      <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Note</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="note"></textarea>
+                      </div>
+                      <div class="submit-booking">
+                        <input type="submit" class="btn btn-primary" value="Complete Order" name="order_click"></input>
+                      </div>
+                    </fieldset>
+                  </form>
+                  <div class="col-md-3"></div>
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+
+
+          <!-- END OF CUSTOMER DETAIL FORM  -->
+
+
+
+
+
+
         </div>
-      <?php } else { ?>
-
-      <?php } ?>
-      <div class="booking">
-    <div class="row">
-        <div class="col-md-5"></div>
-        <div class="col-md-2">
-            <h3 class="booking-title">Contact Information</h3>
-            
-        </div>
-       
-        
-    <div class="row">
-    <div class="col-md-3"></div>
-        <div class="col-md-6">
-        <form id="cart-form" action="cart.php?action=submit">
-                <fieldset>
-                  <div class="form-group">
-                    <label for="" class="p-2">Name</label>
-                    <input type="text" id="" class="form-control" placeholder="Name" name="name">
-                  </div>
-                  <div class="form-group">
-                    <label for="" class="p-2">Phone</label>
-                    <input type="text" id="" class="form-control" placeholder="Phone" name="phone">
-                  </div>
-                  <div class="form-group">
-                  <label for="" class="p-2">Address</label>
-                    <input type="text" id="" class="form-control" placeholder="123 Lesmurine" name="address">
-                  </div>
-                
-                  <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Note</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="note"></textarea>
-                  </div>
-                  <div class="submit-booking">
-                    <input type="submit" class="btn btn-primary" value="Complete Order" name="order_click"></input>
-                  </div>
-                </fieldset>
-              </form>
-              <div class="col-md-3"></div>
-            </div>
-        
-            </div>
-            </div>
-      
-    </div>
 
 
-    <!-- END OF CUSTOMER DETAIL FORM  -->
+        <?php include 'footer.php'; ?>
+    </div><!-- END OF CONTAINER -->
 
-
-
-
-
-
-  </div>
-
-
-  <?php include 'footer.php'; ?>
-  </div><!-- END OF CONTAINER -->
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+      crossorigin="anonymous"></script>
 
 
 </body>
