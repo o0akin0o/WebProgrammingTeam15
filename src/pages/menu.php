9ebb7c38e87a84f48855dbbc881799d78f8581f5
dandev947366 <dan.dev947366@gmail.com>
2024-02-12 22:30:38 +0200

    update

--- src/pages/menu.php
+++ src/pages/menu.php
@@ -26,35 +26,9 @@ setcookie("prev_page", $prevPage, time() + 3600, "/");
     <div class="container mx-auto my-4">
         <?php include 'header.php'; ?>
         <?php include('second_nav.php'); ?>
-        <!-- SEARCH & SORT FOOD BY PRICE -->
-       
-
-        <div class="container">
+        <?php include('search_sort.php'); ?>
 
 
-            <div class="row mt-2">
-                <div class="col-sm-3 col-md-3">
-                    <form action="" method="GET">
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Your dishes" aria-label="Search"
-                                aria-describedby="button-addon2" name="name">
-                            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
-
-                        </div>
-                    </form>
-                </div>
-                <div class="col-sm-7 col-md-7"></div>
-                <div id="filter-box" class="col-sm-2 col-md-2">
-                    <select id="sort-box" class="form-select" aria-label="Default select example"
-                        onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
-                        <option selected>Price Sorting</option>
-                        <option value="?field=price&sort=asc">Ascending</option>
-                        <option value="?field=price&sort=desc">Descending</option>
-                    </select>
-                </div>
-            </div>
-        </div>
-        <!-- END SEARCH & SORTING PRICE -->
         <div class="container">
             <form action="" class="quick-buy-form" method="post">
                 <div class="row my-4 mt-4">
