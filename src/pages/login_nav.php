145d57bf37a443fd3134dba391e59e871dc77040
o0akin0o <lenhatminhquan@gmail.com>
2024-02-15 03:59:45 +0200

    update link to admin page

--- src/pages/login_nav.php
+++ src/pages/login_nav.php
@@ -1,15 +1,18 @@
+<?php
+    $loggedIn = isset($_COOKIE["name"]);
+    $isAdmin = isset($_COOKIE["Admin"]) && $_COOKIE["Admin"] === "admin";
 
-            <?php
-                $loggedIn = isset($_COOKIE["name"]);
-                if ($loggedIn) {
-                    echo '<span class="p-3 link-success">Welcome, ' . $_COOKIE['name'] . '</span>';
-                    echo '<a id="logout" class="p-3 link-secondary" href="logout.php"><i class="fa fa-sign-out">Logout</i></a>';
-                } else {
-            
-                echo '<a id="login" class="p-1 link-success" href="login.php"><i class="fa-solid fa-right-to-bracket">Login</i></a>';
-                echo'/';
-                echo '<a id="login" class="p-1 link-success text-muted" href="create_account.php"><i class="fa-solid fa-user-plus">Create Account</i></a>';
-                
-               
-                }
-            ?>
+    if ($loggedIn) {
+        echo '<span class="p-3 link-success">Welcome, ' . $_COOKIE['name'] . '</span>';
+        echo '<a id="profile" class="p-3 link-secondary" href="profile.php"><i class="fa fa-user"></i> Profile</a>';
+        if ($isAdmin) {
+            echo '<a id="admin" class="p-3 link-secondary" href="admin.php"><i class="fa fa-user-shield"></i> Admin</a>';
+        }
+        echo '<a id="logout" class="p-3 link-secondary" href="logout.php"><i class="fa fa-sign-out"></i> Logout</a>';
+        
+    } else {
+        echo '<a id="login" class="p-1 link-success" href="login.php"><i class="fa-solid fa-right-to-bracket"></i> Login</a>';
+        echo '/';
+        echo '<a id="login" class="p-1 link-success text-muted" href="create_account.php"><i class="fa-solid fa-user-plus"></i> Create Account</a>';
+    }
+?>
