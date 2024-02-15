cf3985d51fe1b62af654e5b9744928d1da06d91b
dandev947366 <dan.dev947366@gmail.com>
2024-02-15 11:23:07 +0200

    update

---
+++ src/pages/admin_header.php
@@ -0,0 +1,21 @@
+<nav class="navbar row">
+                <h2 class="col-md-4 col-sm-4 logo">Eastern Kitchen</h2>
+                <div class="col-md-8 col-sm-8">
+
+                    <ul>
+                    <li>
+                            <a class="nav-link" href="home.php">Home</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="admin.php">Admin</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="report.php">Report</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="update.php">Update</a>
+                        </li>
+                     
+                    </ul>
+                </div>
+            </nav>
